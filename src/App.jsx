4fe9a39d7f7178f9cd7353d3cc52102bac30070f
Abhi-hertz <93651229+AE-Hertz@users.noreply.github.com>
2024-10-08 19:01:20 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+
+import AcceptToggleButton from "./Component/acceptToggle";
+
+function App() {
+    return (
+        <>
+            <AcceptToggleButton/>
+        </>
+    );
+}
+
+export default App;
